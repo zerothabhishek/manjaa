cff8c865d2b09dfae60892a85b05e5b0d4586447
abhishek <bigbeliever@gmail.com>
2011-04-24 15:05:05 +0530

    test setup

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -5,4 +5,19 @@ class PostTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "preprocess should create a file for the post in _posts" do
+    post = Post.make
+    post.preprocess
+  end
+  
+  test "preprocess should place the YAML front matter in the _posts file" do
+  end
+  
+  test "preprocess should place the post content in the _posts file " do
+  end
+  
+  test "proprocess should mark the post as processed at last" do
+  end
+  
 end
